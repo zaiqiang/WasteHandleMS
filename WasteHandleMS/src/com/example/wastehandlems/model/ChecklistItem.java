902a671605e57b3bcc49f5248bcd493d7b4c4532
jwu <jwu@xogrp.com>
2014-10-13 11:38:32 +0800

    add

---
+++ WasteHandleMS/src/com/example/wastehandlems/model/ChecklistItem.java
@@ -0,0 +1,17 @@
+package com.example.wastehandlems.model;
+
+import java.util.List;
+
+public class ChecklistItem  {
+
+	String aa="";
+
+	public String getAa() {
+		return aa;
+	}
+
+	public void setAa(String aa) {
+		this.aa = aa;
+	}
+
+}
